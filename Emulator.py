8a9b499cebc2cead02cb9cb81a0b0f4a89c212d1
Cupiditas <ligert123@hotmail.com>
2020-09-07 12:22:13 +1000

    Removed the emulator being a class

--- Emulator.py
+++ Emulator.py
@@ -20,49 +20,49 @@ def getTime():
 def setTimeMultiplier(val):
     global time_multiplier
     time_multiplier = val
-class Emulator:
-    #Emulator -> Program
-    #returns conductivity of blood sensor (INT)
-    def getConductivity(self):
-        return 0
-    #returns if the reservoir is connected (BOOL)
-    def reservoirConnected(self):
-        return False
-    #returns the level of the reservoir (INT)
-    def reservoirLevel(self):
-        return 0
-    #returns the results of the blood sensor self test (BOOL)
-    def bloodSensorFunctional(self):
-        return False
-    #returns the result of the pump self test (BOOL)
-    def pumpFunctional(self):
-        return False
-    #returns the voltage of the battery (INT)
-    def batteryVoltage(self):
-        return 0
-    #returns the conductivity of the liquid in the needle (INT)
-    def needleInternalConductivity(self):
-        return 0
-    #returns if the needle is connected (BOOL)
-    def needleConnected(self):
-        return False
-    #returns the state of the manual button
-    def manualButton(self):
-        return False
 
-    #Program -> Emulator
-    #starts a self test of the pump
-    def selfTestPump(self):
-        return False
-    #starts a self test of the blood sensor
-    def selfTestBloodSensor(self):
-        return False
-    #makes the pump inject 10mL of insulin
-    def activatePump(self):
-        return False
-    #sets the state of the alarm
-    def alarmSetState(self,state):
-        return False
-    #write text to the display at position (x,y)
-    def displayWrite(self,text,x,y):
-        return False
+#Emulator -> Program
+#returns conductivity of blood sensor (INT)
+def getConductivity():
+    return 0
+#returns if the reservoir is connected (BOOL)
+def reservoirConnected():
+    return False
+#returns the level of the reservoir (INT)
+def reservoirLevel():
+    return 0
+#returns the results of the blood sensor self test (BOOL)
+def bloodSensorFunctional():
+    return False
+#returns the result of the pump self test (BOOL)
+def pumpFunctional():
+    return False
+#returns the voltage of the battery (INT)
+def batteryVoltage():
+    return 0
+#returns the conductivity of the liquid in the needle (INT)
+def needleInternalConductivity():
+    return 0
+#returns if the needle is connected (BOOL)
+def needleConnected():
+    return False
+#returns the state of the manual button
+def manualButton():
+    return False
+
+#Program -> Emulator
+#starts a self test of the pump
+def selfTestPump():
+    return False
+#starts a self test of the blood sensor
+def selfTestBloodSensor():
+    return False
+#makes the pump inject 10mL of insulin
+def activatePump():
+    return False
+#sets the state of the alarm
+def alarmSetState(state):
+    return False
+#write text to the display at position (x,y)
+def displayWrite(text,x,y):
+    return False
