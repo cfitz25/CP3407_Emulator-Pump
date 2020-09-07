74c8b4cbfa68330ce853b9a939a82ff7382a75e0
Cupiditas <ligert123@hotmail.com>
2020-09-07 13:28:26 +1000

    Added more to loop5second

--- PumpProgram.py
+++ PumpProgram.py
@@ -28,33 +28,47 @@ class PumpProgram:
             #run if it has been atleast 30 seconds since last run
             if(tmp_time-last_time_5s >= 5):
                 last_time_5s = tmp_time
-                loop5Second()
+                self.loop5Second()
             #run if it has been atleast 30 seconds since last run
             if(tmp_time-last_time_30s >= 30):
                 last_time_30s = tmp_time
-                loop30Second()
+                self.loop30Second()
             #run if it has been atleast 10 minutes since last run
             if(tmp_time-last_time_10min >= 10*60):
                 last_time_10min = tmp_time
-                loop10Minute()
+                self.loop10Minute()
             time.sleep(1)
         return
     def loop5Second(self):
+        #display blood sugar levels
         displayWrite("sugar:\r\n"+str(self.sugar_level)+" cc/L", self.DISPPOS_SUGAR)
-        displayWrite("left:\r\n"+str(self.sugar_level)+"mL", self.DISPPOS_SUGAR)
-        displayWrite("total:\r\n"+str(self.sugar_level)+"mL", self.DISPPOS_SUGAR)
-        displayWrite("battery:\r\n"+str(self.sugar_level)+"%", self.DISPPOS_SUGAR)
+        #display how much is left in the reservoir
+        displayWrite("left:\r\n"+str(self.sugar_level)+"mL", self.DISPPOS_RESERV)
+        #display how much insulin has been given today
+        displayWrite("total:\r\n"+str(self.sugar_level)+"mL", self.DISPPOS_TOTAL_INSULIN)
+        #display battery left
+        displayWrite("battery:\r\n"+str(self.sugar_level)+"%", self.DISPPOS_BATTERY)
+        #if the last injection has occured display it otherwise display dummy info
         write_string = "last injection:\r\n"
         if(self.last_injection[0] != -1):
-            write_sring += str(self.last_injection[1])+"mL\r\n("+str(getTimeString(self.last_injection[0]))+")"
+            dt = getDatetime(self.last_injection[0])
+
+            write_sring += str(self.last_injection[1])+"mL\r\n("+str(dt)+")"
         else:
             write_sring += "--mL\r\n()"
-        displayWrite(write_string, self.DISPPOS_SUGAR)
+        displayWrite(write_string, self.DISPPOS_LAST_INJECT)
         write_string = "("
+        #if messages exist then display it otherwise display dummy info
         if(len(self.messages) > 0):
             time,message = self.messages[self.message_ind]
-            
-        displayWrite("sugar:\r\n"+str(self.sugar_level)+" cc/L", self.DISPPOS_SUGAR)
+            self.message_ind += 1
+            if(self.message_ind >= len(self.messages)):
+                self.message_ind = 0
+            dt = getDatetime(time)
+            write_string += str(datetime.time())+") "+str(message)
+        else:
+            write_string += ") ---------------"
+        displayWrite(write_string, self.DISPPOS_MESSAGES)
         return
     def loop30Second(self):
         print("LOOP 30 SEC: ",getTime())
@@ -74,4 +88,6 @@ class PumpProgram:
     def loop10Minute(self):
         print("LOOP 10 MIN: ",getTime())
         return
-mainLoop()
+p = PumpProgram()
+setTimeMultiplier(5)
+p.mainLoop()
