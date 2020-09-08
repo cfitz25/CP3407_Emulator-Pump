791b12d2ea5c438f2f695971a704ec8e919c9c5a
yuvibernstein <yuvi_bernstein@hotmail.com>
2020-09-08 12:23:12 +1000

    initial commit

---
+++ app/src/main/java/au/edu/jcu/cp3406/pumpappv1/MainActivity.java
@@ -0,0 +1,25 @@
+package au.edu.jcu.cp3406.pumpappv1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void menuClicked(View view) {
+        Intent intent = new Intent(this, MenuActivity.class);
+        startActivityForResult(intent, MenuActivity.MENU_REQUEST);
+    }
+
+
+}
---
+++ app/src/main/java/au/edu/jcu/cp3406/pumpappv1/MenuActivity.java
@@ -0,0 +1,15 @@
+package au.edu.jcu.cp3406.pumpappv1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MenuActivity extends AppCompatActivity {
+    public static final int MENU_REQUEST = 123;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
