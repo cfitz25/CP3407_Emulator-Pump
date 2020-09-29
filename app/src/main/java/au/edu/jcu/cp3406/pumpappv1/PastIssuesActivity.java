f045a899470366ac7e94245a684602ba9299d0ac
yuvibernstein <yuvi_bernstein@hotmail.com>
2020-09-30 09:17:38 +1000

    completed all button functionality

--- app/src/main/java/au/edu/jcu/cp3406/pumpappv1/PastIssuesActivity.java
+++ app/src/main/java/au/edu/jcu/cp3406/pumpappv1/PastIssuesActivity.java
@@ -12,5 +12,6 @@ public class PastIssuesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_past_issues);
+
     }
 }
